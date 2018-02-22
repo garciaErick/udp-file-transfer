5ed5d89031d4f62524461af81bb2ce37e6a18440
Erick Garcia <garcia.erick.cs@gmail.com>
2018-02-22 02:59:52 -0700

    Refractored code, it is now more understandable and instead of sending messages it first splits everything into packets and sends them

--- stopWait/client/udpClient.py
+++ stopWait/client/udpClient.py
@@ -4,52 +4,22 @@ import sys
 import re
 import os
 
+# default params
+serverAddr = ('localhost', 50000)
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+protocol = ""
+file_name = ""
+
 
 def usage():
     print "usage: %s [--serverAddr host:port]" % sys.argv[0]
 
 
-def get_method(file_name):
-    # clientSocket = socket(AF_INET, SOCK_DGRAM)
-    # send_handshake(clientSocket)
-    recieve_packets(file_name, clientSocket)
-
-
-def recieve_packets(file_name, clientSocket):
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    with open("stopWait/client/getFromServer.txt", 'w') as outputFile:
-        while 1:
-            try:
-                print "fuck da pliz"
-                message, serverAddrPort = clientSocket.recvfrom(2048)
-                outputFile.write(message + "\n")
-                outputFile.flush()
-            finally:
-                message = "Successfully made get request"
-
-def put_method(file_name):
-    print("Initializing PUT from client")
-    send_packets(file_name, clientSocket)
-
-
-# def send_handshake(clientSocket):
-#     message = "Trying to start handshake from client"
-#     print message
-#     clientSocket.sendto(message, serverAddr)
-#     modifiedMessage, clientAddrPort = clientSocket.recvfrom(2048)
-#     if (modifiedMessage == "Acknowledging handshake from server"):
-#         print "Successfully initiated communication with server\n"
-#     else:
-#         print modifiedMessage
-#         # Send on timeout
-#         sys.exit(1)
-
 def send_protocol_and_fname(clientSocket, protocol, file_name):
-    print "Starting protocol: %s, file: %s" % (protocol.upper(), file_name)
+    print "Starting protocol from client: %s, file: %s" % (protocol.upper(), file_name)
     message = protocol + " " + file_name
     clientSocket.sendto(message, serverAddr)
     modified_message, serverAddrPort = clientSocket.recvfrom(2048)
-    print modified_message
     if (modified_message == "Acknowledging handshake from server"):
         print "Successfully initiated communication with server\n"
     else:
@@ -57,12 +27,38 @@ def send_protocol_and_fname(clientSocket, protocol, file_name):
         # Send on timeout
         sys.exit(1)
 
-def send_packets(file_name, clientSocket):
+
+def get_method(file_name):
+    clientSocket = socket(AF_INET, SOCK_DGRAM)
+    file_name = "stopWait/client/getFromServer.txt"
+    with open(file_name, 'w') as outputFile:
+        while 1:
+            try:
+                print "fuck da pliz"
+                packet, serverAddrPort = clientSocket.recvfrom(2048)
+                outputFile.write(packet + "\n")
+                outputFile.flush()
+            finally:
+                message = "Successfully made get request"
+                clientSocket.sendto(message, serverAddrPort)
+
+
+def put_method(file_name):
+    print "Initializing PUT from client"
+    # send_packets(file_name, clientSocket)
+    packets_to_send = split_into_packets(file_name)
+    for packet in packets_to_send:
+        clientSocket.sendto(packet, serverAddr)
+    print "Sucessfully finished PUT request"
+
+
+def split_into_packets(file_name):
     size = os.path.getsize(file_name)
     counter = 0
     i = 0
     k = 100
     message = ""
+    packets = list()
     with open(file_name, 'r') as inputFile:
         while counter < size:
             if (size - counter < 100):
@@ -72,53 +68,47 @@ def send_packets(file_name, clientSocket):
                 i += 1
                 counter += 1
             i = 0
-        clientSocket.sendto(message, serverAddr)
-        print "Message from %s is: %s" % (repr(serverAddr), message)
-
-#TODO:
-#def split_into_packets(file_name):
+        packets.append(message)
+    return packets
 
 
 # TODO:
 # def retransmit_on_timeout():
 
+def main():
+    try:
+        args = sys.argv[1:]
+        while args:
+            sw = args[0]
+            del args[0]
+            if sw == "--serverAddr":
+                addr, port = re.split(":", args[0])
+                del args[0]
+                serverAddr = (addr, int(port))
+            elif sw == "--protocol" or sw == "-p":
+                protocol = args[0]
+                del args[0]
+            elif sw == "--file_name" or sw == "-f":
+                file_name = args[0]
+                del args[0]
+            else:
+                print "unexpected parameter %s" % args[0]
+                usage()
 
-# default params
-serverAddr = ('localhost', 50000)
-protocol = ""
-file_name = ""
-
-# TODO: research on this for better args parsing https://docs.python.org/3.3/library/argparse.html
-try:
-    args = sys.argv[1:]
-    while args:
-        sw = args[0]
-        del args[0]
-        if sw == "--serverAddr":
-            addr, port = re.split(":", args[0])
-            del args[0]
-            serverAddr = (addr, int(port))
-        elif sw == "--protocol" or sw == "-p":
-            protocol = args[0]
-            del args[0]
-        elif sw == "--file_name" or sw == "-f":
-            file_name = args[0]
-            del args[0]
+        clientSocket = socket(AF_INET, SOCK_DGRAM)
+        if protocol.lower() == "put":
+            send_protocol_and_fname(clientSocket, protocol, file_name)
+            put_method(file_name)
+        elif protocol.lower() == "get":
+            send_protocol_and_fname(clientSocket, protocol, file_name)
+            get_method(file_name)
         else:
-            print "unexpected parameter %s" % args[0]
+            print "Invalid protocol: %s" % protocol
             usage()
-
-    clientSocket = socket(AF_INET, SOCK_DGRAM)
-    if protocol.lower() == "put":
-        send_protocol_and_fname(clientSocket, protocol, file_name)
-        put_method(file_name)
-    elif protocol.lower() == "get":
-        send_protocol_and_fname(clientSocket, protocol, file_name)
-        get_method(file_name)
-    else:
-        print "Invalid protocol: %s" % protocol
+    except Exception as e:
+        print "An exception ocurred " + "\n" + str(e)
         usage()
-except Exception as e:
-    print "An exception ocurred " + "\n"  + str(e)
-    usage()
-    sys.exit(1)
+        sys.exit(1)
+
+
+main()
--- stopWait/server/udpServer.py
+++ stopWait/server/udpServer.py
@@ -7,52 +7,11 @@ import os
 # default params
 serverAddr = ("", 50001)
 
+
 def usage():
     print "usage: %s [--serverPort <port>]" % sys.argv[0]
     sys.exit(1)
 
-
-def split_into_packets(file_name, clientAddrPort):
-    # todo: estoy aqui
-    size = os.path.getsize(file_name)
-    counter = 0
-    i = 0
-    k = 100
-    message = ""
-    with open(file_name, 'r') as inputFile:
-        while counter < size:
-            if (size - counter < 100):
-                k = size - counter
-            while i < k:
-                message += inputFile.read(counter)
-                i += 1
-                counter += 1
-            i = 0
-        serverSocket.sendto(message, clientAddrPort)
-        print "Message from %s is: %s" % (repr(clientAddrPort), message)
-
-
-# TODO:
-# def retransmit_on_duplicate():
-
-# At least one side must implement retransmit-on-timeout; otherwise a lost packet leads to deadlock as the sender
-# and the receiver both wait forever. The other side must implement at least one of retransmit-on-duplicate or
-# retransmit-on-timeout; usually the former alone. If both sides implement retransmit-on-timeout with different
-# timeout values, generally the protocol will still work.
-
-
-# def receive_handshake(serverSocket):
-#     message, clientAddrPort = serverSocket.recvfrom(2048)
-#     if (message == "Trying to start handshake from client"):
-#         modifiedMessage = "Acknowledging handshake from server"
-#         print modifiedMessage
-#         serverSocket.sendto(modifiedMessage, clientAddrPort)
-#         print "Successfully initiated communication with client"
-
-
-def get_method(file_name, clientAddrPort):
-    print "ready to send"
-    split_into_packets(file_name,clientAddrPort)
 
 def receive_protocol_and_fname(serverSocket):
     message, clientAddrPort = serverSocket.recvfrom(2048)
@@ -74,17 +33,53 @@ def receive_protocol_and_fname(serverSocket):
     print file_name
 
 
+def split_into_packets(file_name):
+    size = os.path.getsize(file_name)
+    counter = 0
+    i = 0
+    k = 100
+    message = ""
+    packets = list()
+    with open(file_name, 'r') as inputFile:
+        while counter < size:
+            if (size - counter < 100):
+                k = size - counter
+            while i < k:
+                message += inputFile.read(counter)
+                i += 1
+                counter += 1
+            i = 0
+        packets.append(message)
+    return packets
+
+
+# TODO:
+# def retransmit_on_duplicate():
+
+# At least one side must implement retransmit-on-timeout; otherwise a lost packet leads to deadlock as the sender
+# and the receiver both wait forever. The other side must implement at least one of retransmit-on-duplicate or
+# retransmit-on-timeout; usually the former alone. If both sides implement retransmit-on-timeout with different
+# timeout values, generally the protocol will still work.
+
+
+def get_method(file_name, clientAddrPort):
+    print "ready to send"
+    packets_to_send = split_into_packets(file_name)
+    for packet in packets_to_send:
+        serverSocket.sendto(packet, clientAddrPort)
+
+
 def put_method(file_name):
-    print "ready to receive"
     with open("stopWait/server/putFromClient.txt", 'w') as outputFile:
         while 1:
             try:
-                message, clientAddrPort = serverSocket.recvfrom(2048)
-                outputFile.write(message + "\n")
+                packet, clientAddrPort = serverSocket.recvfrom(2048)
+                outputFile.write(packet + "\n")
                 outputFile.flush()
             finally:
                 message = "Successfully made put request"
                 serverSocket.sendto(message, clientAddrPort)
+
 
 try:
     args = sys.argv[1:]
@@ -105,5 +100,3 @@ print "binding datagram socket to %s" % repr(serverAddr)
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(serverAddr)
 receive_protocol_and_fname(serverSocket)
-# put_method()
-# get_method()
