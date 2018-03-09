697f589c8971c99d514f431099726d5bf76ef9e1
student <student@localhost.localdomain>
2018-03-08 23:47:31 -0700

    finished dissector

--- brogrammerDissector.lua
+++ brogrammerDissector.lua
@@ -1,15 +1,15 @@
--- adrian protocol example
+-- Brocode Protocol
 -- author: Adrian Veliz (Original Code)
 -- author: Erick Garcia
 -- author: Fernando Martinez
 
 
-local adrian_proto = Proto("brocode","Bro Code")
+local brocode_proto = Proto("brocode","Bro Code")
 
 -- create a function to dissect it
-function adrian_proto.dissector(buffer,pinfo,tree)
+function brocode_proto.dissector(buffer,pinfo,tree)
     pinfo.cols.protocol = "BROCODE"
-    local subtree = tree:add(adrian_proto,buffer(),"BroCode Protocol Data")
+    local subtree = tree:add(brocode_proto,buffer(),"BroCode Protocol Data")
 
 	-- Message is at least three bytes long
 	if buffer:len() < 3 then
@@ -26,48 +26,30 @@ function adrian_proto.dissector(buffer,pinfo,tree)
 
     if command == "GET" or command == "PUT" then -- Request a file
 	myProtocol =  command
-	subtree:add(buffer(2), "FILE: " .. buffer(3):string())
+	myFile = buffer(3):string()
+	myStatus = "Innitiating protocol"
     elseif fullMessage == "Received last packet" or fullMessage == "Finished!" then -- Request a file
-	subtree:add("Finished " .. myProtocol .. " request")
+	myStatus = "Finished " .. myProtocol .. " request"
     elseif fullMessage == "Acknowledging handshake from server" then -- Request a file
-	subtree:add("Started communication")
-    elseif fullMessage == "Acknowledging handshake from server" then -- Request a file
-	subtree:add("Started communication")
-    else						-- Unknown message type	
+	myStatus = "Started Communication"
+    elseif string.match(fullMessage,"Recieved packet") then -- Request a file
+	myStatus = "Recieved packet " .. fullMessage:sub(17,17)
+    else --Unkown message, do not print Protocol || File || Status
+	subtree:add("Probably processing file...")
         subtree:add_expert_info(PI_PROTOCOL, PI_WARN, "Unknown message type")
         subtree:add(buffer(0),"ERROR: " .. buffer(0))
 	unknownMessage  = true
     end
-    if unknownMessage  == false then
-    	subtree:add(buffer(0,1),"Sequence Number: " .. seq_num)
-     	subtree:add(buffer(1,1),"Type: " .. msg_type)
-    	subtree:add(buffer(0,3),"command: " .. command)
-    	subtree:add("Full Message: " .. fullMessage)
+    if unknownMessage  == false then --Valid message, print everything 
+	subtree:add("Protocol: " .. myProtocol)
+	subtree:add("FILE: " .. myFile)
+	subtree:add("STATUS: " .. myStatus)
     end
 end
 -- load the udp.port table
 udp_table = DissectorTable.get("udp.port")
-tcp_table = DissectorTable.get("tcp.port")
 -- register protocol to handle udp ports
-udp_table:add(5000,adrian_proto)
-udp_table:add(5001,adrian_proto) 
-udp_table:add(5002,adrian_proto)
-udp_table:add(50000,adrian_proto)
-udp_table:add(50001,adrian_proto)
-udp_table:add(50002,adrian_proto)
-tcp_table:add(5000,adrian_proto)
+udp_table:add(50000,brocode_proto)
+udp_table:add(50001,brocode_proto)
+udp_table:add(50002,brocode_proto)
  
--- original source code and getting started
--- https://shloemi.blogspot.com/2011/05/guide-creating-your-own-fast-wireshark.html
-
--- helpful links
--- https://delog.wordpress.com/2010/09/27/create-a-wireshark-dissector-in-lua/
--- https://wiki.wireshark.org/LuaAPI/Tvb
--- http://lua-users.org/wiki/LuaTypesTutorial
--- https://wiki.wireshark.org/Lua/Examples
--- https://wiki.wireshark.org/LuaAPI/Proto
--- https://www.wireshark.org/docs/wsdg_html_chunked/wslua_dissector_example.html
--- https://www.wireshark.org/lists/wireshark-users/201206/msg00010.html
--- https://wiki.wireshark.org/LuaAPI/TreeItem
--- https://www.wireshark.org/docs/man-pages/tshark.html
-
